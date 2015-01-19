05fe06b2ccdf1d0551957029470fe4c2e1262fa3
Elana Hashman <elana@hashman.ca>
2015-01-18 19:05:09 -0500

    Modify 'Save As' URLs for nicer linking site-wide

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,8 +15,22 @@ EXTRA_PATH_METADATA = {
         {'path': 'favicon.ico'},
 }
 
-ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}/index.html'
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+CATEGORY_URL = '{slug}/'
+CATEGORY_SAVE_AS = '{slug}/index.html'
+
+TAGS_SAVE_AS = 'tags/index.html'
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tags/{slug}/index.html'
+
+AUTHOR_URL = 'author/{slug}/'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+
 
 TIMEZONE = 'Canada/Eastern'
 
